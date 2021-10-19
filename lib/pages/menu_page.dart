a4b996229fefa92b82bae6926a237ad005b3dfe7
Hitbee-dev <ckim.esw@gmail.com>
2021-10-19 15:42:04 +0900

    homepage layout code recyle

--- lib/pages/menu_page.dart
+++ lib/pages/menu_page.dart
@@ -1,10 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:wireless_mouse/pages/home_page.dart';
 
 class MenuPage extends StatelessWidget {
   const MenuPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("${(Get.arguments as IPStatus).ip}"),
+      ),
+      body: Container(
+        child: Center(
+          child: Column(
+            children: [
+              ElevatedButton(
+                  onPressed: () {}, child: Container(child: Text("TouchPad"))),
+              ElevatedButton(
+                  onPressed: () {}, child: Container(child: Text("Camera"))),
+              ElevatedButton(
+                  onPressed: () {}, child: Container(child: Text("HardWare"))),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
